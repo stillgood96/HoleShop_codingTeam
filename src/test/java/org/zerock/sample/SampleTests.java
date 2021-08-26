d3064863a9a2b8006b8e4ce23b818eca71b8ba43
imlsw96 <a787574@naver.com>
2021-08-26 13:49:27 +0900

    web.xml / servlet-context / root-contex에 대한 주석 설명 추가

--- src/test/java/org/zerock/sample/SampleTests.java
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -10,7 +10,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertNotNull;
 
+// 현재 테스트 코드가 스프링을 실행하는 역할을 할 것이라는 것을 나타내주는 Annotation
 @RunWith(SpringJUnit4ClassRunner.class)
+// 지정된 클래스나 문자열을 이용해서 필요한 객체들을 스프링 내에 객체로 등록한다. (스프링의 빈으로 등록된다 )
+// servlet-context가 아닌 root-context로 지정된 이유는 servlet-context의 경우 웹 클라이언트 요청, view 관련에 대해 빈을 설정해주고
+// root-context는 뷰와 관련이 없는 DB, service레이어의 빈을 설정하는데 여기서 테스트는 서비스 쪽담당..이니까 ?
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class SampleTests {
