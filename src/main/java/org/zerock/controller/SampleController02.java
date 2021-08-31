15aab446832c5ab951157f8c4a7cacef31653ca1
stillgood96 <a787574@naver.com>
2021-08-31 23:59:43 +0900

    129page / Controller의 파라미터 수집

--- src/main/java/org/zerock/controller/SampleController02.java
+++ src/main/java/org/zerock/controller/SampleController02.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
 
 // 126 page
 @Controller
@@ -22,5 +24,24 @@ public class SampleController02 {
     public void basicGet2() {
 
         log.info("basic get only get........");
+    }
+
+    // 129page
+    // Controller의 파라미터 수집 테스트
+    // Controller를 작성할 때 가장 편리한 기능은 파라미터가 자동으로 수집되는 기능이다.
+    //매번 request.getParameter()를 이용하는 불편함을 없앨 수 있다.
+    @GetMapping("/ex01")
+    public String ex01(SampleDTO dto) {
+        log.info("" + dto);
+
+        return "ex01";
+    }
+
+    @GetMapping("/ex02")
+    public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+        log.info("name" + name);
+        log.info("age" + age);
+
+        return "ex02";
     }
 }
---
+++ src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,11 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+// Controller의 파라미터 수집 테스트
+
+@Data
+public class SampleDTO {
+    private String name;
+    private int age;
+}
